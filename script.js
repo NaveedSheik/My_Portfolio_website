336b5087b663aeeb20b7a8fed48fcb87d794bcc4
NaveedSheik <m.navid4u@gmail.com>
2022-11-16 19:04:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,141 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //*********** Night mode
+    
+    const toggleSwitch =
+    document.querySelector('.theme-slider input[type="checkbox"]');
+ 
+/* Function to change theme */
+function switchTheme(e) {
+ 
+    /* Once checkbox is checked default theme change to dark */
+    if (e.target.checked) {
+        document.documentElement.setAttribute('theme', 'dark');
+    }
+ 
+    /* While page in dark mode and checkbox is
+    checked then theme back to change light*/
+    else {
+        document.documentElement.setAttribute('theme', 'light');
+    }
+}
+ 
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+
+    /*============ Qualification Tab ========*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+tab.addEventListener('click', () =>{
+  const target = document.querySelector(tab.dataset.target)
+
+  tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('qualification__active')
+  })
+  target.classList.add('qualification__active')
+
+  tabs.forEach(tab =>{
+      tab.classList.remove('qualification__active')
+  })
+  tab.classList.add('qualification__active')
+})
+})
+
+/*============ Accordion skills ========*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills()
+{
+    let itemClass = this.parentNode.className  
+
+    for(i = 0; i < skillsContent.length; i++)
+    {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close')
+    {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+
+
+});
